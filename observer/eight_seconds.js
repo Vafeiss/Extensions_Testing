e7cc63da54fdc17c73e9247695dcff69bcb1bcc1
Giorgos-Chiras <166930497+Giorgos-Chiras@users.noreply.github.com>
2026-03-12 16:40:51 +0200

    refactor/ Refactor code to use reusable functions

--- observer/eight_seconds.js
+++ observer/eight_seconds.js
@@ -7,36 +7,40 @@ import { get_differences_between_strings } from '../helper/dom_diff.js';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
-(async () => {
+export async function compare_dom_with_extension(extension_name, url = "https://example.com") {
 
-    const no_extension_browser =  await puppeteer.launch({
-        headless: false});
+    const no_extension_browser = await puppeteer.launch({
+        headless: true
+    });
 
     const page = await no_extension_browser.newPage();
-    await page.goto("https://example.com", {waitUntil: "domcontentloaded" });
+    await page.goto(url, { waitUntil: "domcontentloaded" });
+
     const dom = await page.evaluate(() => document.documentElement.outerHTML);
     fs.writeFileSync("CleanDOM", dom);
+
     await no_extension_browser.close();
 
-    const extensionPath = resolve(__dirname, "../InjectScript");
+    const extension_path = resolve(__dirname, "../extensions/" + extension_name);
 
     const extension_browser = await puppeteer.launch({
-        headless: false, args: [
-            `--disable-extensions-except=${extensionPath}`,
-            `--load-extension=${extensionPath}`,]
+        headless: true,
+        args: [
+            `--disable-extensions-except=${extension_path}`,
+            `--load-extension=${extension_path}`,
+        ]
     });
 
     const extension_page = await extension_browser.newPage();
-    await extension_page.goto("https://example.com", {waitUntil: "domcontentloaded" });
+    await extension_page.goto(url, { waitUntil: "domcontentloaded" });
 
     await new Promise(resolve => setTimeout(resolve, 8000));
+
     const modified_dom = await extension_page.evaluate(() => document.documentElement.outerHTML);
 
     fs.writeFileSync("ModifiedDOM", modified_dom);
 
     await extension_browser.close();
-    const result = get_differences_between_strings(dom, modified_dom);
-    console.log("Added: ", result.added);
-    console.log("Removed:", result.removed);
 
-})();
+    return get_differences_between_strings(dom, modified_dom);
+}
