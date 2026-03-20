1f2592a874b2e73f0aed2f7107d4681dab0046c6
Giorgos-Chiras <166930497+Giorgos-Chiras@users.noreply.github.com>
2026-03-20 15:21:58 +0200

    Added extensions

--- generateFingerprints.js
+++ generateFingerprints.js
@@ -1,44 +1,56 @@
-import {analyzeExtensionChanges} from "./analyzeDOMs.js";
-import {createFingerprint} from "./helper/createFingerprint.js";
-import {insert} from "./database/insert.js";
-import {getFingerprints} from "./database/getFingerprint.js";
-import {getExtension} from "./database/getExtension.js";
-import {connect} from "./database/connect.js";
-import {getExtensionName} from "./helper/getExtensionName.js";
+import { analyzeExtensionChanges } from "./analyzeDOMs.js";
+import { createFingerprint } from "./helper/createFingerprint.js";
+import { insert } from "./database/insert.js";
+import { getFingerprints } from "./database/getFingerprint.js";
+import { getExtension } from "./database/getExtension.js";
+import { connect } from "./database/connect.js";
+import { getExtensionName } from "./helper/getExtensionName.js";
 
 const conn = await connect();
-const extensions = ["clockext","backgroundChanger", "ChangeWords"]
+const extensions = [
+  "difoiogjjojoaoomphldepapgpbgkhkb/5.25.8_0",
+  "eiimnmioipafcokbfikbljfdeojpcgbh/7.0.0_0",
+  "eimadpbcbfnmbkopoojfekhnkhdbieeh/4.9.121_0",
+  "ghbmnnjooekpmoecnnnilnnbdlolhkhi/1.101.1_0",
+  "gighmmpiobklfepjocnamgkkbiglidom/6.37.1_0",
+  "heifjnlfhcndklgdnbjkailkipkfgkpm/2.9_0",
+  "nllcnknpjnininklegdoijpljgdjkijc/9.19.0_0",
+  "nmmhkkegccagdldgiimedpiccmgmieda/1.0.0.6_0",
+  "oppflpnigmhkldmdmmbnopidlhahanji/2.2.36_0",
+];
+
 for (const identity of extensions) {
-    /**
-     * Read the name from manifest.json
-     */
-    const name = getExtensionName(identity);
-    console.log(name)
+  /**
+   * Read the name from manifest.json
+   */
+  const name = getExtensionName(identity);
+  console.log(name);
 
-    /**
-     * Analyze DOM with and without extension
-     * Remove any dynamic content
-     * Create a single string fingerprint
-     */
-    const result = await analyzeExtensionChanges(identity);
-    const fingerprint = createFingerprint(result);
+  /**
+   * Analyze DOM with and without extension
+   * Remove any dynamic content
+   * Create a single string fingerprint
+   */
+  const result = await analyzeExtensionChanges(identity);
+  const fingerprint = createFingerprint(result);
 
-    /**
-     * Ensure that a fingerprint was generated
-     */
-    if (fingerprint.length <= 2) {
-        console.log("Fingerprint not found.");
-        continue
-    }
+  /**
+   * Ensure that a fingerprint was generated
+   */
+  if (fingerprint.length <= 5) {
+    await insert(conn, identity, name, "No DOM Changes");
 
-    /**
-     * Insert details in DB
-     */
-    await insert(conn,identity,name, fingerprint);
+    continue;
+  }
 
-    //Validation
-    console.log("\nFingerprint: " + await getFingerprints(conn,identity));
-    console.log("\nName: " + await getExtension(conn,fingerprint));
+  /**
+   * Insert details in DB
+   */
+  await insert(conn, identity, name, fingerprint);
+
+  //Validation
+  console.log("\nFingerprint: " + (await getFingerprints(conn, identity)));
+  console.log("\nName: " + (await getExtension(conn, fingerprint)));
 }
 
 conn.end();
--- helper/getExtensionName.js
+++ helper/getExtensionName.js
@@ -1,16 +1,30 @@
 import fs from "fs";
 import path from "path";
-import {fileURLToPath} from "url";
+import { fileURLToPath } from "url";
 
 export function getExtensionName(identity) {
-    const manifestPath = fileURLToPath(
-        new URL(`../extensions/${identity}/manifest.json`, import.meta.url)
-    );
-    try {
-        const data = fs.readFileSync(manifestPath, "utf-8");
-        return JSON.parse(data).name;
-    } catch (error) {
-        console.log(error);
-    }
+  var manifestPath = fileURLToPath(
+    new URL(`../extensions/${identity}/manifest.json`, import.meta.url),
+  );
+  try {
+    var data = fs.readFileSync(manifestPath, "utf-8");
+    var name = JSON.parse(data).name;
+    if (name.includes("MSG")) {
+      manifestPath = fileURLToPath(
+        new URL(
+          `../extensions/${identity}/_locales/en/messages.json`,
+          import.meta.url,
+        ),
+      );
+      data = fs.readFileSync(manifestPath, "utf-8");
 
+      name = JSON.parse(data).appName.message;
+      if (name != null) return name;
+      else return "undefined";
+    } else {
+      return name;
+    }
+  } catch (error) {
+    console.log(error);
+  }
 }
